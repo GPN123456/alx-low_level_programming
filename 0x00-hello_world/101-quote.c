484e5014bc37e0112d45c6440759438bdfc7f95f
Patra <darlingpatty1@gmail.com>
2022-07-05 15:17:20 -0700

    101-quote.c added

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,7 +10,6 @@
 
 int main(void)
 {
-printf("\"and that piece of art is useful\" - Dora Korpar, 2015-10-19");
-fprintf(stderr, "");
+printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 return (1);
 }
