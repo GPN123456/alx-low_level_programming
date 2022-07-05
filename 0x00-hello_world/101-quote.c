770d97447990a3f21634db370e0777068c3c348d
Patra <darlingpatty1@gmail.com>
2022-07-05 15:06:28 -0700

    101-quote.c added

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+/**
+ *main - Entry point
+ *
+ *Description:This program prints a message
+ *
+ *Return: 1
+ */
+
+int main(void)
+{
+printf("\"and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+return (1);
+}
