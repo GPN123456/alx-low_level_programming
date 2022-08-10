9ec892f0ee5245a0b4626913e73c9a881d1daa58
Patra <darlingpatty1@gmail.com>
2022-08-10 05:14:56 -0700

    funvtion

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * rev_string - update value.
+ * @s: value to be evaluate.
+ *
+ * Return: not.
+ */
+void rev_string(char *s)
+{
+	int len = 0;
+	int l = 0;
+	char *y = s;
+	int e = 0;
+	int x;
+	char n;
+
+		{
